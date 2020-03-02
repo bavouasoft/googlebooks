6875b123a53d9353b3e26a3bac465dbeb2684858
Achille Bavoua <bavouasoft@yahoo.fr>
2020-03-02 18:39:49 -0500

    modified readme file

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,17 +1,7 @@
-const path = require("path");
 const router = require("express").Router();
-const booksRoutes = require("./books");
-const googleRoutes = require("./google");
+const bookRoutes = require("./books");
 
-// Books routes match /api/books
-router.use("/books", booksRoutes);
-
-// Google Routes match /api/books
-router.use("/google", googleRoutes);
-
-// For anything else, render the html page
-router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../../client/build/index.html"));
-});
+// Book routes
+router.use("/books", bookRoutes);
 
 module.exports = router;
